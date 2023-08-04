8b1cefa3b17bea8aadffbf53391c33457663d17c
Zzebastian <sebastian.scheverin@gmail.com>
2023-08-04 00:42:18 -0300

    Calculadora Terminada

--- Calculadora.py
+++ Calculadora.py
@@ -1,15 +1,22 @@
 import os
 import tkinter as tk
 from tkinter import ttk
+import tkinter.font as tkFont
 
 os.system('cls')
 
-primerNumero = None
-segundoNumero = None
-evitarSuperposicion = False
+def borrar():
+    global primerNumero, segundoNumero, evitarSuperposicion, tareaprevia
+    primerNumero = None
+    segundoNumero = None
+    evitarSuperposicion = False
+    tareaprevia = ''
+    entrada.delete('1.0', 'end')
+#
 
 def colocarNumero(numero):
     global evitarSuperposicion
+    
     if evitarSuperposicion != False:
         entrada.delete('1.0', 'end')
         evitarSuperposicion = False
@@ -17,76 +24,90 @@ def colocarNumero(numero):
     entrada.insert(tk.END, numero)
 # 
 def operacion(tarea):
-    global primerNumero, segundoNumero, evitarSuperposicion
-    if primerNumero == None:
-        primerNumero = int(entrada.get('1.0', 'end'))
+    global primerNumero, segundoNumero, evitarSuperposicion, tareaprevia
+    
+    if tareaprevia != '':
+        segundoNumero = int(entrada.get('1.0', 'end'))
+        accion(tareaprevia)
+        tareaprevia = tarea
 
+        if tarea == 'igual':
+            tareaprevia = ''
+            primerNumero = None
+            segundoNumero = None
+
+    elif primerNumero == None:
+        try:
+            primerNumero = int(entrada.get('1.0', 'end'))
+        except:
+            primerNumero = 0
+        
+        tareaprevia = tarea
+        print('1')
     else:
         segundoNumero = int(entrada.get('1.0', 'end'))
-        accion(tarea)
-        entrada.delete('1.0', 'end')
-        entrada.insert(tk.END, primerNumero)
-        print(segundoNumero)
-        segundoNumero = None
-
-    evitarSuperposicion = True
-    print(primerNumero)
-# 
-def accion(tarea):
+        tareaprevia = tarea
+        print('2')
+    
+    evitarSuperposicion = True   
+#
+def accion(tareaprevia):
     global primerNumero, segundoNumero
-
-    if tarea == 'sumar':
+    print(primerNumero, segundoNumero, tareaprevia)##### Continuar acá cuando decida las tareaprevias
+    if tareaprevia == 'sumar':
         primerNumero += segundoNumero
-    elif tarea == 'restar':
+    elif tareaprevia == 'restar':
         primerNumero -= segundoNumero
-    elif tarea == 'multiplicar':
+    elif tareaprevia == 'multiplicar':
         primerNumero *= segundoNumero
-    elif tarea == 'dividir':
+    elif tareaprevia == 'dividir':
         primerNumero /= segundoNumero
     
+    entrada.delete('1.0', 'end')
+    entrada.insert(tk.END, primerNumero)
+
 #
-def igual():
-    print('igual')
-# 
 
 
-botonesNro = [{'texto': '0', 'fg': 'black', 'funcion': lambda: colocarNumero(0),'col': 1, 'fila': 4, 'tf': 1, 'tc': 2},
-        {'texto': '1', 'fg': 'black', 'funcion': lambda: colocarNumero(1),'col': 1, 'fila': 3, 'tf': 1, 'tc': 1},
-        {'texto': '2', 'fg': 'black', 'funcion': lambda: colocarNumero(2),'col': 2, 'fila': 3, 'tf': 1, 'tc': 1},
-        {'texto': '3', 'fg': 'black', 'funcion': lambda: colocarNumero(3),'col': 3, 'fila': 3, 'tf': 1, 'tc': 1},
-        {'texto': '4', 'fg': 'black', 'funcion': lambda: colocarNumero(4),'col': 1, 'fila': 2, 'tf': 1, 'tc': 1},
-        {'texto': '5', 'fg': 'black', 'funcion': lambda: colocarNumero(5),'col': 2, 'fila': 2, 'tf': 1, 'tc': 1},
-        {'texto': '6', 'fg': 'black', 'funcion': lambda: colocarNumero(6),'col': 3, 'fila': 2, 'tf': 1, 'tc': 1},
-        {'texto': '7', 'fg': 'black', 'funcion': lambda: colocarNumero(7),'col': 1, 'fila': 1, 'tf': 1, 'tc': 1},
-        {'texto': '8', 'fg': 'black', 'funcion': lambda: colocarNumero(8),'col': 2, 'fila': 1, 'tf': 1, 'tc': 1},
-        {'texto': '9', 'fg': 'black', 'funcion': lambda: colocarNumero(9),'col': 3, 'fila': 1, 'tf': 1, 'tc': 1},
-        {'texto': '+', 'fg': 'black', 'funcion': lambda: operacion('sumar'),'col': 4, 'fila': 1, 'tf': 1, 'tc': 1},
-        {'texto': '-', 'fg': 'black', 'funcion': lambda: operacion('restar'),'col': 4, 'fila': 2, 'tf': 1, 'tc': 1},
-        {'texto': 'x', 'fg': 'black', 'funcion': lambda: operacion('multiplicar'),'col': 4, 'fila': 3, 'tf': 1, 'tc': 1},
-        {'texto': '/', 'fg': 'black', 'funcion': lambda: operacion('dividir'),'col': 4, 'fila': 4, 'tf': 1, 'tc': 1},
-        {'texto': '=', 'fg': 'black', 'funcion': lambda: operacion('igual'),'col': 3, 'fila': 4, 'tf': 1, 'tc': 1},
+
+botonesNro = [{'texto': '0', 'funcion': lambda: colocarNumero(0),'col': 2, 'fila': 4, 'tf': 1, 'tc': 1},
+        {'texto': '1', 'funcion': lambda: colocarNumero(1),'col': 1, 'fila': 3, 'tf': 1, 'tc': 1},
+        {'texto': '2', 'funcion': lambda: colocarNumero(2),'col': 2, 'fila': 3, 'tf': 1, 'tc': 1},
+        {'texto': '3', 'funcion': lambda: colocarNumero(3),'col': 3, 'fila': 3, 'tf': 1, 'tc': 1},
+        {'texto': '4', 'funcion': lambda: colocarNumero(4),'col': 1, 'fila': 2, 'tf': 1, 'tc': 1},
+        {'texto': '5', 'funcion': lambda: colocarNumero(5),'col': 2, 'fila': 2, 'tf': 1, 'tc': 1},
+        {'texto': '6', 'funcion': lambda: colocarNumero(6),'col': 3, 'fila': 2, 'tf': 1, 'tc': 1},
+        {'texto': '7', 'funcion': lambda: colocarNumero(7),'col': 1, 'fila': 1, 'tf': 1, 'tc': 1},
+        {'texto': '8', 'funcion': lambda: colocarNumero(8),'col': 2, 'fila': 1, 'tf': 1, 'tc': 1},
+        {'texto': '9', 'funcion': lambda: colocarNumero(9),'col': 3, 'fila': 1, 'tf': 1, 'tc': 1},
+        {'texto': '+', 'funcion': lambda: operacion('sumar'),'col': 4, 'fila': 1, 'tf': 1, 'tc': 1},
+        {'texto': '-', 'funcion': lambda: operacion('restar'),'col': 4, 'fila': 2, 'tf': 1, 'tc': 1},
+        {'texto': 'x', 'funcion': lambda: operacion('multiplicar'),'col': 4, 'fila': 3, 'tf': 1, 'tc': 1},
+        {'texto': '/', 'funcion': lambda: operacion('dividir'),'col': 4, 'fila': 4, 'tf': 1, 'tc': 1},
+        {'texto': '=', 'funcion': lambda: operacion('igual'),'col': 3, 'fila': 4, 'tf': 1, 'tc': 1},
+        {'texto': 'C', 'funcion': lambda: borrar(),'col': 1, 'fila': 4, 'tf': 1, 'tc': 1},
 ]
 
 
 
 
-
 calculadora = tk.Tk()
-calculadora.title('Básica')
-calculadora.geometry('400x300')
+calculadora.title('Calculadora Básica')
+calculadora.geometry('350x200')
 
 # Inicialización Filas y Columnas por si eventualmente se desea mover el conjunto
 i = 0 # Columnas
 j = 0 # Filas
 
-entrada = tk.Text(calculadora, height = 1, width = 30)
+
+entrada = tk.Text(calculadora, height = 2, width = 30)
 entrada.grid(column = 1+i, row = 1+j, columnspan= 4)
 j += 1
 
 for bt in botonesNro:
-    boton = ttk.Button(text= bt['texto'], command= bt['funcion'] )
-    boton.grid(column= bt['col']+i, row= bt['fila']+j,columnspan= bt['tc'], rowspan= bt['tf'])
+    boton = ttk.Button(text= bt['texto'], command= bt['funcion'])
+    boton.grid(column= bt['col']+i, row= bt['fila']+j,columnspan= bt['tc'], rowspan= bt['tf'], )
 
-
+borrar()
 
 tk.mainloop()
